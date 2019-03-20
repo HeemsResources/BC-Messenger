67712289bce0230ea9f897034d33d0ba359f932a
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-03-20 12:53:47 -0400

    tab position

--- Account.java
+++ Account.java
@@ -1,19 +1,20 @@
 //super class of so we can manipulae Accounts
 public class Account{
-  private String firstName;
-  private String lastName;
-  private String emplID;
-  private String phoneNumber;
-  private String email;
-  private Address address;
-  private String username;
-  private String password;
-  private String yearin;
-  private String friends;
-  private int age;
-  public Account(){
+      private String firstName;
+      private String lastName;
+      private String emplID;
+      private String phoneNumber;
+      private String email;
+      private Address address;
+      private String username;
+      private String password;
+      private String yearin;
+      private String friends;
+      private int age;
 
-  }
+      public Account(){
+      }
+
   public Account(String emplID){
     this.emplID=emplID;
   }
